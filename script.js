c08ad55f722965b2a34e2a3b6470ff37c4fa4544
Hasan <120640600+hasan-bro-coder@users.noreply.github.com>
2023-12-25 22:46:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,29 @@ let line = 1
 let lineval = ""
 let lineend = false
 let word = ""
+let dlog = document.querySelector(".endscreen")
 
 
 window.onload = () => {
+	run()
+}
+document.querySelector(".new-up").onclick = () => {
+	dlog.querySelector("h2 span").innerHTML = word
+	dlog.showModal()
+}
+
+document.querySelector(".new").onclick = () => {
+	dlog.close()
+	location.reload()
+}
+
+function run(){
 	document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "green"
 	worker.postMessage({ word: "", type: "get" });
 
 	window.addEventListener("keydown", (e) => {
 		let val = e.key
-		
+
 		console.log(e.key)
 		if (val == "Enter") {
 			if (!lineend) {
@@ -22,28 +36,28 @@ window.onload = () => {
 			}
 			worker.postMessage({ word: lineval.toLowerCase(), type: "find" });
 			// cursor++
-		} 
+		}
 		else if (val == "Backspace") {
 			if (cursormin == cursor) return 0;
 			document.querySelector(".grid .col:nth-child(" + cursor + ")").innerText = ""
 			lineval = lineval.slice(0, -1);
-				document.querySelector(".grid .col:nth-child(" + (cursor) + ")").style.borderColor = "green"
+			document.querySelector(".grid .col:nth-child(" + (cursor) + ")").style.borderColor = "green"
 			cursor--
-				document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "white"
+			document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "white"
 			// if((cursor+1) % 5 != 0){
-					// }else{
-					// 	document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
-					// }
+			// }else{
+			// 	document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
+			// }
 			lineend = false
-		} 
-		else if (!lineend && val.match(/^[a-zA-Z]+$/)) {
+		}
+		else if (!lineend && val.match(/^[a-zA-Z]+$/) && val.length == 1) {
 			val = val.toUpperCase()
 			lineval += val
 			document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").innerText = val
-			if((cursor+1) % 5 != 0){
-		document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "green"
-		document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
-			}else{
+			if ((cursor + 1) % 5 != 0) {
+				document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "green"
+				document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
+			} else {
 				document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
 			}
 			cursor++
@@ -55,10 +69,10 @@ window.onload = () => {
 		}
 		console.log(cursor)
 	})
-	
+
 	document.querySelectorAll(".key").forEach((el, i) => {
 		el.addEventListener("click", (evt) => {
-			let val = el.innerText
+			let val = el.dataset.key
 			if (val == "↵") {
 				if (!lineend) {
 					alert("too small bro")
@@ -66,27 +80,27 @@ window.onload = () => {
 				}
 				worker.postMessage({ word: lineval.toLowerCase(), type: "find" });
 				// cursor++
-			} 
+			}
 			else if (val == "-") {
 				if (cursormin == cursor) return 0;
 				document.querySelector(".grid .col:nth-child(" + cursor + ")").innerText = ""
 				lineval = lineval.slice(0, -1);
-					document.querySelector(".grid .col:nth-child(" + (cursor) + ")").style.borderColor = "green"
+				document.querySelector(".grid .col:nth-child(" + (cursor) + ")").style.borderColor = "green"
 				cursor--
-					document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "white"
+				document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "white"
 				// if((cursor+1) % 5 != 0){
-						// }else{
-						// 	document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
-						// }
+				// }else{
+				// 	document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
+				// }
 				lineend = false
-			} 
+			}
 			else if (!lineend) {
 				lineval += val
 				document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").innerText = val
-				if((cursor+1) % 5 != 0){
-			document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "green"
-			document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
-				}else{
+				if ((cursor + 1) % 5 != 0) {
+					document.querySelector(".grid .col:nth-child(" + (cursor + 2) + ")").style.borderColor = "green"
+					document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
+				} else {
 					document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "white"
 				}
 				cursor++
@@ -114,65 +128,106 @@ window.onload = () => {
 	worker.onmessage = (e) => {
 		if (e.data.type == "find") {
 			if (e.data.val) {
+				let ellist = []
+				// var tl = anime.timeline({
+				//   easing: 'easeOutExpo',
+				//   duration: 1000
+				// });
 				for (let i = cursormin; i < cursor; i++) {
 					let el = document.querySelector(".grid .col:nth-child(" + (i + 1) + ")")
-					// ellist.push(el)
+					ellist.push(el)
 					let reallineval = lineval.toUpperCase()
 					console.log(reallineval)
 					lineval = lineval.toLowerCase()
 					console.log(lineval.includes(word[i - cursormin]), lineval[i - cursormin], word[i - cursormin], i, cursormin)
 					if (lineval[i - cursormin] == word[i - cursormin]) {
 						el.style.backgroundColor = "green"
-						document.querySelector("[data-key='" + reallineval[i - cursormin] + "']").style.backgroundColor =  "green"
-						anime({
-							targets: el,
-							rotateX: 360*2,
-							duration: 1500,
-							// color: "#0000",
-							backgroundColor: {
-								value:['#303030',"green"],
-								duration: 500,
-							},
-							easing: 'easeInOutSine',
-							delay: (i - cursormin ) * 100
-						})
-					} else if (word.includes(lineval[i - cursormin])) {
-						el.style.backgroundColor = "#838300"
-						document.querySelector("[data-key='" + reallineval[i - cursormin] + "']").style.backgroundColor =  "#838300"
+						document.querySelector("[data-key='" + reallineval[i - cursormin] + "']").style.backgroundColor = "green"
+						if (lineval != word) {
 						anime({
 							targets: el,
 							rotateX: 360,
-							duration: 1000,
+							duration: 1500,
 							// color: "#0000",
-							backgroundColor:{ 
-								value:['#303030',"#838300"],
+							backgroundColor: {
+								value: ['#242729', "green"],
 								duration: 500,
 							},
 							easing: 'easeInOutSine',
-							delay: (i - cursormin ) * 100
+							delay: (i - cursormin) * 100
+						})
+						}
+					} else if (word.includes(lineval[i - cursormin])) {
+						el.style.backgroundColor = "#838300"
+						document.querySelector("[data-key='" + reallineval[i - cursormin] + "']").style.backgroundColor = "#838300"
+						anime({
+							targets: el,
+							keyframes: [
+								{ rotateX: 90 },
+								{ rotateX: 0 },
+							],
+							duration: 1000,
+							// color: "#0000",
+							backgroundColor: {
+								value: ['#242729', "#838300"],
+								duration: 500,
+							},
+							easing: 'easeInOutSine',
+							delay: (i - cursormin) * 100
 						})
 					} else {
 						el.style.backgroundColor = "gray"
-						document.querySelector("[data-key='" + reallineval[i - cursormin] + "']").style.backgroundColor =  "gray"
-				anime({
-					targets: el,
-					rotateX: 360,
-					duration: 1000,
-					// color: "#0000",
-					backgroundColor: { 
-						value:['#303030','#646464'],
-						duration: 500,
-					},
-					easing: 'easeInOutSine',
-					delay: (i - cursormin )* 100
-				})
+						document.querySelector("[data-key='" + reallineval[i - cursormin] + "']").style.backgroundColor = "gray"
+						anime({
+							targets: el,
+							// rotateX: 360,
+							duration: 1000,
+							keyframes: [
+								{ rotateX: 90 },
+								{ rotateX: 0 },
+							],
+							// color: "#0000",
+							backgroundColor: {
+								value: ['#242729', '#646464'],
+								duration: 500,
+							},
+							easing: 'easeInOutSine',
+							delay: (i - cursormin) * 100
+						})
 					}
+				}
+				if (lineval == word) {
+					dlog.querySelector("h1").innerHTML = `you win hehe`
+					dlog.querySelector("p").innerHTML = `- hasan`
+					dlog.querySelector("h2 span").innerHTML = word
+					anime({
+						targets: ellist,
+						duration: 1000,
+						keyframes: [
+							{ translateY: -40 },
+							{ translateY: 0 }
+						],
+						easing: 'easeInOutSine',
+						delay: anime.stagger(100),
+						complete: function(anim) {
+							
+							dlog.showModal()
+						  }
+					})
+					return 0
+				}
+				else if(line == 6){
+					// alert("you lose")
+					dlog.querySelector("h2 span").innerHTML = word
+					dlog.showModal()
+					return 0
+					
 				}
 				cursormin += 5
 				lineval = ""
 				line += 1
 				lineend = false
-				document.querySelector(".grid .col:nth-child(" + (cursor+1) + ")").style.borderColor = "green"
+				document.querySelector(".grid .col:nth-child(" + (cursor + 1) + ")").style.borderColor = "green"
 				// alert("exists")
 			} else {
 				alert("word not found")
